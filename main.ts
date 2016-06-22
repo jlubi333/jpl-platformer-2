5a86eede35d14c53324e3fd44b0e53960c1828a8
Justin Lubin <jlubi333@gmail.com>
2016-06-22 12:34:39 -0400

    More work.

--- main.ts
+++ main.ts
@@ -1,167 +1,274 @@
-(function() {
-    /*
-     * Constants
-     */
-    const MAX_FRAME_TIME = 0.25;
-    const DT = 1/60;
+/*
+ * Constants
+ */
+const MAX_FRAME_TIME = 0.25;
+const DT = 1/60;
 
-    /*
-     * Interfaces
-     */
-    interface Updatable {
-        update(dt: number): void;
+/*
+ * Interfaces
+ */
+interface Updatable {
+    update(dt: number): void;
+}
+
+interface Renderable {
+    render(ctx: CanvasRenderingContext2D): void;
+}
+
+/*
+ * Utilities
+ */
+
+class Vector {
+    static zero = new Vector(0, 0);
+    constructor(public x: number, public y: number) {}
+}
+
+class BoundingBox {
+    constructor(
+        public x: number,
+        public y: number,
+        public width: number,
+        public height: number
+    ) {}
+
+    setPosition(x: number, y: number): void {
+        this.x = x;
+        this.y = y;
     }
 
-    interface Renderable {
-        render(ctx: CanvasRenderingContext2D): void;
+    setSize(width: number, height: number): void {
+        this.width = width;
+        this.height = height;
     }
 
-    /*
-     * Game
-     */
-    class Game implements Updatable, Renderable {
-        updateList: Array<Updatable>;
-        renderList: Array<Renderable>;
-
-        constructor(
-            updateList: Array<Updatable>,
-            renderList: Array<Renderable>
-        ) {
-            this.updateList = updateList;
-            this.renderList = renderList;
-        }
-
-        update(dt) {
-            this.updateList.forEach(u => u.update(dt));
-        }
-
-        render(ctx) {
-            this.renderList.forEach(r => r.render(ctx));
-        }
+    right(): number {
+        return this.x + this.width;
     }
 
-    /*
-     * Tile
-     */
-    class Tile implements Renderable {
-        static size = 24;
-
-        id: number
-        row: number;
-        col: number;
-        x: number;
-        y: number;
-
-        constructor(id: number, row: number, col: number) {
-            this.id = id;
-            this.row = row;
-            this.col = col;
-            this.x = col * Tile.size;
-            this.y = row * Tile.size;
-        }
-
-        render(ctx) {
-            if (this.id == 1) {
-                ctx.strokeRect(this.x, this.y, Tile.size, Tile.size);
-            }
-        }
+    bottom(): number {
+        return this.y + this.height;
     }
 
-    /*
-     * Main Loop
-     */
-    let previousTime = 0;
-    let accumulator = 0;
-    function loop(
-        currentTime: number,
+    intersects(other: BoundingBox): boolean {
+        return !(this.x        >= other.right()  ||
+                 this.right()  <= other.x        ||
+                 this.y        >= other.bottom() ||
+                 this.bottom() <= other.y);
+    }
+}
+
+/*
+ * DrawUtil
+ */
+class DrawUtil {
+    static drawFilledStrokedRect(
         ctx: CanvasRenderingContext2D,
-        game: Game
+        fillColor: string,
+        strokeColor: string,
+        x: number,
+        y: number,
+        width: number,
+        height: number,
+        strokeWidth: number
     ) {
-        const frameTime = Math.min((currentTime - previousTime) / 1000,
-                                   MAX_FRAME_TIME);
-        accumulator += frameTime;
-
-        while (accumulator >= DT) {
-            accumulator -= DT;
-            game.update(DT);
-        }
-
-        game.render(ctx);
-
-        previousTime = currentTime;
-        window.requestAnimationFrame(
-            (timestamp) => loop(timestamp, ctx, game)
-        );
+        ctx.fillStyle = fillColor;
+        ctx.fillRect(x, y, width, height);
+        ctx.strokeStyle = strokeColor;
+        ctx.lineWidth = strokeWidth;
+        ctx.strokeRect(x, y, width, height);
     }
+}
 
-    /*
-     * Main
-     */
-    function main() {
-        const gameCanvas = <HTMLCanvasElement> document.getElementById("game");
+/*
+ * Game
+ */
+class Game implements Updatable, Renderable {
+    constructor(
+        public tiles: Tile[][],
+        public entities: Entity[]
+    ) {}
 
-        gameCanvas.width = window.innerWidth;
-        gameCanvas.height = window.innerHeight;
-
-        const ctx = gameCanvas.getContext("2d");
-
-		const map = [
-			[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-			[1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2],
-			[1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-			[1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-			[1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-			[1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0],
-			[1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-			[1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0],
-			[1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-			[1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-			[1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0],
-			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-			[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-			[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0],
-			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
-			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
-			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-			[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-			[1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		];
-
-        let renderList: Array<Renderable> = [];
-
-        for (let r = 0; r < map.length; r++) {
-            for (let c = 0; c < map[r].length; c++) {
-                renderList.push(new Tile(map[r][c], r, c));
+    update(dt: number): void {
+        this.entities.forEach(e => {
+            e.update(dt);
+            if (e.bb.y > this.tiles.length * Tile.size) {
+                e.die();
             }
-        }
-
-        window.requestAnimationFrame(
-            (timestamp) => loop(timestamp, ctx, new Game([], renderList))
-        );
+        });
     }
 
-    window.onload = main;
-})();
+    render(ctx: CanvasRenderingContext2D): void {
+        this.tiles.forEach(row => row.forEach(col => col.render(ctx)));
+        this.entities.forEach(e => e.render(ctx));
+    }
+}
+
+/*
+ * Tile
+ */
+class Tile implements Renderable {
+    static size = 24;
+
+    public x: number;
+    public y: number;
+
+    constructor(
+        public id: number,
+        public row: number,
+        public col: number
+    ) {
+        this.id = id;
+        this.row = row;
+        this.col = col;
+        this.x = col * Tile.size;
+        this.y = row * Tile.size;
+    }
+
+    render(ctx: CanvasRenderingContext2D): void {
+        if (this.id == 1) {
+            ctx.strokeRect(this.x, this.y, Tile.size, Tile.size);
+        }
+    }
+}
+
+/*
+ * Entity
+ */
+abstract class Entity implements Updatable, Renderable {
+    private finished: boolean;
+
+    constructor(
+        public colorString: string,
+        public bb: BoundingBox,
+        public velocity: Vector
+    ) {
+        this.finished = false;
+    }
+
+    abstract render(ctx: CanvasRenderingContext2D): void;
+
+    update(dt: number): void {
+        this.updatePosition(dt);
+        this.handleEvents();
+    }
+
+    updatePosition(dt: number): void {
+        this.bb.x += this.velocity.x * dt;
+        this.bb.y += this.velocity.y * dt;
+    }
+
+    handleEvents(): void {
+        this.getCollidingEntities().forEach(e => this.onEntityCollide(e));
+    }
+
+    onEntityCollide(sender: Entity): void {
+    }
+
+    die(): void {
+        this.finished = true;
+    }
+
+    getCollidingEntities(): Entity[] {
+        return [];
+    }
+}
+
+/*
+ * Main Loop
+ */
+let previousTime = 0;
+let accumulator = 0;
+function loop(
+    currentTime: number,
+    ctx: CanvasRenderingContext2D,
+    game: Game
+): void {
+    const frameTime = Math.min((currentTime - previousTime) / 1000,
+                               MAX_FRAME_TIME);
+    accumulator += frameTime;
+
+    while (accumulator >= DT) {
+        accumulator -= DT;
+        game.update(DT);
+    }
+
+    game.render(ctx);
+
+    previousTime = currentTime;
+    window.requestAnimationFrame((timestamp) => {
+        loop(timestamp, ctx, game)
+    });
+}
+
+/*
+ * Main
+ */
+function main(): void {
+    const gameCanvas = <HTMLCanvasElement> document.getElementById("game");
+
+    gameCanvas.width = window.innerWidth;
+    gameCanvas.height = window.innerHeight;
+
+    const ctx = gameCanvas.getContext("2d");
+
+    const map = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2],
+        [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+        [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+        [1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0],
+        [1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+
+    let tiles: Tile[][] = [[]];
+
+    for (let r = 0; r < map.length; r++) {
+        for (let c = 0; c < map[r].length; c++) {
+            tiles[r].push(new Tile(map[r][c], r, c));
+        }
+        tiles.push([]);
+    }
+
+    window.requestAnimationFrame((timestamp) => {
+        loop(timestamp, ctx, new Game(tiles, []))
+    });
+}
+
+window.onload = main;
